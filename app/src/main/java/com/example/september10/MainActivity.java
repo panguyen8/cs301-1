560cfe6ea96d812267f3aafb42630e7efce78f41
nguyenph22 <liuer21@up.edu>
2019-09-12 08:15:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/september10/MainActivity.java
@@ -0,0 +1,18 @@
+
+package com.example.september10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SurfaceView mySurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+
+    }
+}
---
+++ app/src/main/java/com/example/september10/MySurfaceView.java
@@ -0,0 +1,36 @@
+package com.example.september10;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.util.AttributeSet;
+
+public class MySurfaceView extends SurfaceView {
+
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setWillNotDraw(false);
+    }
+
+    public void onDraw(Canvas canvas) {
+        /*
+        Paint paint = new Paint();
+        paint.setColor(Color.MAGENTA);
+        //paint.setStyle(Paint.Style.Stroke);
+        paint.setStrokeWidth(5.0f);
+
+        canvas.drawRect(100, 200, 200, 400, paint);
+        */
+
+        Bitmap myImageBMP = BitmapFactory.decodeResource(getResources(), R.drawable.pigeon);
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(myImageBMP, 100, 10, null);
+        Log.i("MySurfaceView.onDraw", "I just drew the image!");
+    }
+}
